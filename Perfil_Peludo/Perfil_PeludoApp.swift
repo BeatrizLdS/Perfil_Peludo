214f97ef3645c50b6ed9f30c6cd5496430b61d06
BeatrizLdS <beatriz_.silva@hotmail.com>
2022-05-13 10:23:34 -0300

    Initial Commit

---
+++ Perfil_Peludo/Perfil_PeludoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Perfil_PeludoApp.swift
+//  Perfil_Peludo
+//
+//  Created by Beatriz Leonel da Silva on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Perfil_PeludoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
