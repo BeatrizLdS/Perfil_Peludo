838acdcf2a05e4db64eccaf388c4e0fb3a486e9f
BeatrizLdS <beatriz_.silva@hotmail.com>
2022-05-17 11:44:02 -0300

    Implemented Pet temperaments

---
+++ Perfil_Peludo/Views/PetTemperament.swift
@@ -0,0 +1,17 @@
+//
+//  PetTemperament.swift
+//  Perfil_Peludo
+//
+//  Created by Beatriz Leonel da Silva on 17/05/22.
+//
+
+import Foundation
+
+enum PetTemperament : String, CaseIterable {
+    
+    case aggressive = "agressivo"
+    case suspicious = "desconfiado"
+    case fearful = "medroso"
+    case calm = "calmo"
+    
+}
